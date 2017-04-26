4b87dea7b3d12ecb5b92688cc251a225e1ab6774
nick ha <nick ha>
2017-04-26 14:26:43 -0700

    modified CL speed control and off mode

--- GP_code/GP_teensy/GP_teensy.ino
+++ GP_code/GP_teensy/GP_teensy.ino
@@ -92,7 +92,7 @@ steerMode mySteerMode = RC_ST;
   int REC_MOTOR = 21; // pin to read signals from reciever
   int throttleValue;
   int topDutyCycleOL = 256*.5; //max speed
-  int buffTx = 10; // buffer for tx input . full range of tx input is 1160 to 1830
+  int buffTxIn = 10; // buffer for tx input . full range of tx input is 1160 to 1830
   int throttleMid = 1500;
   void getSpeedOL(); //gets input from tx, assigns it to a desired speed, determines what to give HI and LI. 
 
@@ -100,10 +100,13 @@ steerMode mySteerMode = RC_ST;
   double vRef;
   double vMeas;
   double vError;
-  double topSpeed = 10; // in ft/sec
+  double vValue; 
+  double topSpeed = 5; // in ft/sec
+  int buffTxOut = 5; // for safety. units of us. (tested)
+  int buffOut = 1; // for safety. larger will mess up linearity . may be in units of speed (ft/sec).(untested)
   int topDutyCycleCL = 256*.5; // max speed 
-  double kpSpeed = .5;
-  double kiSpeed = 0.5;
+  double kpSpeed = 0.5;
+  double kiSpeed = 0.05;
   double vErrorTotal = 0.0;
   void getSpeedCL();
 
@@ -534,8 +537,9 @@ state stateCheck()
   
   //If Tx says to pause
   stateValue = pulseIn(REC_STATE, HIGH, 25000);
-  if (stateValue > 1750 && stateValue < 2000 ) // channel is all the way high
+  if (stateValue > 1550 && stateValue < 2000 ) // channel is all the way high
     {myState = PAUSE;
+    Serial.print(" stateValueTx: ");
     Serial.println(stateValue);
     return myState;}
     
@@ -679,13 +683,14 @@ void getSpeedOL()
       throttleValue = throttleMid;
      }
 
-     motorValue = constrain(map(throttleValue, throttleMid+buffTx,1850, 0, topDutyCycleOL),0,topDutyCycleOL);
-     brakeValue = constrain(map(throttleValue, throttleMid-buffTx,1150, 0, topDutyCycleOL),0,topDutyCycleOL);
+     motorValue = constrain(map(throttleValue, throttleMid+buffTxIn,1850, 0, topDutyCycleOL),0,topDutyCycleOL);
+     brakeValue = constrain(map(throttleValue, throttleMid-buffTxIn,1150, 0, topDutyCycleOL),0,topDutyCycleOL);
      Serial.print("transmitter (1150 to 1850) = " );     
      Serial.print(throttleValue); //integer between 1150 and 1850
 }
 
 // needs a lot of work
+
 void getSpeedCL()
 {
   throttleValue = pulseIn(REC_MOTOR, HIGH, 25000); // throttleValue is a Speed setpoint used for closed loop
@@ -693,26 +698,41 @@ void getSpeedCL()
   {
     throttleValue = throttleMid;
   }
-  
-  vRef = constrain(mapdouble(throttleValue,1150,1850,-topSpeed, topSpeed),-topSpeed,topSpeed); //convert to ft per sec
-  
+  Serial.print(" throttleValue: ");
+  Serial.print(throttleValue);
+
+  if (throttleValue >=  throttleMid)
+  {
+   vRef = constrain(mapdouble(throttleValue, throttleMid+buffTxIn, 1850, 0,topSpeed),0,topSpeed); 
+  }
+  else
+  {
+   vRef = constrain(mapdouble(throttleValue,throttleMid-buffTxIn, 1150, 0,-topSpeed),-topSpeed,0);
+  }
+    
   vMeas = wheelSpeed_R_Copy;
   vError = vRef-vMeas; // in ft/sec
   vErrorTotal += vError;
   Serial.print(" vRef: ");
   Serial.print(vRef);
-  Serial.print(" vError: ");
-  Serial.print(vError);
+  //Serial.print(" vError: ");
+  //Serial.print(vError);
 
-  throttleValue = throttleValue + kpSpeed*vError + kiSpeed*vErrorTotal; //kpSpeed must be in us /(ft/sec)
-
+  //throttleValue = throttleValue + kpSpeed*vError + kiSpeed*vErrorTotal; //kSpeed must be in us /(ft/sec). before they were both .5 
   //throttleValue = constrain(mapdouble(vError,-topSpeed,topSpeed,1000,2000),1000,2000); //convert back to microseconds
   
-  motorValue = constrain(map(throttleValue, throttleMid+buffTx,1850, 0, topDutyCycleCL),0,topDutyCycleCL);
-  brakeValue = constrain(map(throttleValue, throttleMid-buffTx,1150, 0, topDutyCycleCL),0,topDutyCycleCL);
+  //motorValue = constrain(map(throttleValue, throttleMid+buffTxOut,1850, 0, topDutyCycleCL),0,topDutyCycleCL);
+  //brakeValue = constrain(map(throttleValue, throttleMid-buffTxOut,1150, 0, topDutyCycleCL),0,topDutyCycleCL);
   
-  Serial.print(" throttleValue (1150 to 1850) = " );     
-  Serial.print(throttleValue); //integer between 1160 and 1830
+  //Serial.print(" throttleValue (1150 to 1850) = " );     
+  //Serial.print(throttleValue); //integer between 1150 and 1850,
+
+  vValue = vRef + kpSpeed*vError + kiSpeed*vErrorTotal; 
+  Serial.print(" vValue: ");
+  Serial.print(vValue);
+  motorValue = constrain(mapdouble(vValue,0+buffOut,10,0,topDutyCycleCL),0,topDutyCycleCL); //should topSpeed be in here? or should it be a constant?
+  brakeValue = constrain(mapdouble(vValue,0-buffOut,-10,0,topDutyCycleCL),0,topDutyCycleCL);
+  
 }
 
 void steerTx() //needs checking
