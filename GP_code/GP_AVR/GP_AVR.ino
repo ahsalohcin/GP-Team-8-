7eb50013a98b88ea34562a1d4d03917e2988d1cd
Winson <rocketwings@gmail.com>
2017-05-04 23:14:04 -0700

    added arduino code that uses the arduino library

--- GP_code/GP_AVR/GP_AVR.ino
+++ GP_code/GP_AVR/GP_AVR.ino
@@ -6,46 +6,69 @@
 //A0 is connected to ADC0
 int pixels[NUM_LINES][LINE_WIDTH];
 int cur_line = 0;
-DDRD |= (1<<7); //set PortD pin 7 as output, use as SI
-DDRB |= (1<<0); //set PortB pin 0 as output, use as CLK
 
-cli();  //global disable interrupts so that we can set them safely
-//set CS1[2:0] = b101   //set prescaler to clk/1024
-TCCR1B |= (1<<CS12);
-TCCR1B &= ~(1<<CS11);
-TCCR1B |= (1<<CS10);
-//set OCR1A to match value  //set to period in number of 64us (781 for 50ms)
-OCR1A = 781;
-//set WGM1[3:0] = b0100  //set to CTC (Clear Timer on Compare)
-TCCR1B &= ~(1<<WGM13);
-TCCR1B |= (1<<WGM12);
-TCCR1A &= ~(1<<WGM11);
-TCCR1A &= ~(1<<WGM10);
-//set OCIEA in TIMSK1 to 1 to enable interrupt on match
-TIMSK1 |= (1<<OCIEA);
-sei();  //global enable interrupts
+void setup()    {
+    DDRD |= (1<<7); //set PortD pin 7 as output, use as SI
+    DDRD |= (1<<6); //set PortD pin 6 as output, use as SI
+    DDRB |= (1<<0); //set PortB pin 0 as output, use as CLK
+    DDRC |= (1<<0);
 
-void setup()    {}
+    cli();
+    TCCR1B |= (1 << WGM12)|(1 << CS11)|(1 << CS10);
+    // initialize counter
+    TCNT1 = 0;
+    // initialize compare value
+    OCR1A = 24999;
+    // enable compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
+    // enable global interrupts
+    sei();
+
+    /*
+    cli();  //global disable interrupts so that we can set them safely
+    //set CS1[2:0] = b101   //set prescaler to clk/1024
+    TCCR1B |= (1<<CS12);
+    TCCR1B &= ~(1<<CS11);
+    TCCR1B |= (1<<CS10);
+    //set OCR1A to match value  //set to period in number of 64us (781 for 50ms)
+    OCR1A = 1000;
+    //set WGM1[3:0] = b0100  //set to CTC (Clear Timer on Compare)
+    TCCR1B &= ~(1<<WGM13);
+    TCCR1B |= (1<<WGM12);
+    TCCR1A &= ~(1<<WGM11);
+    TCCR1A &= ~(1<<WGM10);
+    //set OCIEA in TIMSK1 to 1 to enable interrupt on match
+    TIMSK1 |= (1<<OCIE1A);
+    //set OCFA on TIFR1 to 1 to enable isr
+    //TIFR1 |= (1<<OCF1A);
+    sei();  //global enable interrupts
+    */
+}
 void loop() {}
 
 ISR(TIMER1_COMPA_vect)  {
     //read whole line from camera
+    PORTC ^= (1<<0);
     PORTD |= (1<<7);    //set SI high
-    PORTB |= (1<<0);    //set CLK high
+    PORTD |= (1<<6);    //set SI high
+    //PORTB |= (1<<0);    //set CLK high
     pixels[cur_line][0] = analogRead(0);    //read 0th pixel from A0
     PORTD &= ~(1<<7);   //set SI low
-    PORTD &= ~(1<<0);   //set CLK low
+    PORTD &= ~(1<<6);   //set SI low
+    //PORTB &= ~(1<<0);   //set CLK low
+    /*
     for(int i=1; i<LINE_WIDTH; i++) {
     PORTB |= (1<<0);    //set CLK high
     pixels[cur_line][i] = analogRead(0);    //read i-th pixel
-    PORTD &= ~(1<<0);   //set CLK low
+    PORTB &= ~(1<<0);   //set CLK low
     }
     //129th pulse
     PORTB |= (1<<0);    //set CLK high
-    PORTD &= ~(1<<0);   //set CLK low
+    PORTB &= ~(1<<0);   //set CLK low
     //increment line count
     cur_line += 1;
     if (cur_line == NUM_LINES)   {
         cur_line = 0;
     }
+    */
 }
---
+++ GP_code/GP_arduino/GP_arduino.ino
@@ -0,0 +1,69 @@
+//#include "TimerOne/TimerOne.h"
+#include "TimerOne.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define LINE_WIDTH 128
+#define NUM_LINES 4
+#define PERIOD 50000 //in microseconds
+#define INTEGRATE_PERCENT 100   //percent of the period with which to integrate the camera
+
+//A0 is connected to ADC0
+uint16_t pixels[NUM_LINES][LINE_WIDTH];
+float send_pixels[LINE_WIDTH];
+uint8_t cur_line = 0;
+uint8_t poll_line = 0; //line it was at since last polled
+uint8_t max = 0;
+int last_time = millis();
+
+void setup()    {
+    DDRD |= (1<<7); //set PortD pin 7 as output, use as SI
+    DDRB |= (1<<0); //set PortB pin 0 as output, use as CLK
+    Timer1.initialize(PERIOD);
+    Timer1.attachInterrupt(capture, PERIOD);
+    Serial.begin(57600);
+    Serial.print('test\n');
+    delay(10);
+    Serial.print('test2\n');
+}
+void loop() {
+    while(millis() > last_time+100)   {
+        last_time = millis();
+        send_data();
+    }
+}
+void capture()  {
+    //read whole line from camera
+    PORTD |= (1<<7);    //set SI high
+    PORTB |= (1<<0);    //set CLK high
+    pixels[cur_line][0] = analogRead(0);    //read 0th pixel from A0
+    PORTD &= ~(1<<7);   //set SI low
+    PORTB &= ~(1<<0);   //set CLK low
+    for(int i=1; i<LINE_WIDTH; i++) {
+    PORTB |= (1<<0);    //set CLK high
+    pixels[cur_line][i] = analogRead(0);    //read i-th pixel
+    PORTB &= ~(1<<0);   //set CLK low
+    }
+    //129th pulse
+    PORTB |= (1<<0);    //set CLK high
+    PORTB &= ~(1<<0);   //set CLK low
+    //increment line count
+    cur_line += 1;
+    if (cur_line == NUM_LINES)   {
+        cur_line = 0;
+    }
+    //compute sum
+    for(int i=0; i<LINE_WIDTH; i++) {
+        send_pixels[i] = send_pixels[i]*(NUM_LINES-1.0)/NUM_LINES + pixels[cur_line][i];
+    }
+}
+
+void send_data()    {
+    //send send_pixels
+    //poll_line = cur_line;
+    for(int i=0; i<LINE_WIDTH; i++) {
+        Serial.print(send_pixels[i], 0);
+        Serial.print(",\t");
+    }
+    Serial.print(';\n');
+
+}
